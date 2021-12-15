bad1284aa4aaf9105d8119ce38b4d9a4cb6d1fee
worramet2678 <95734714+worramet2678@users.noreply.github.com>
2021-12-15 12:04:47 +0700

    15-12-21

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { StyleSheet, Text, View, Alert, SafeAreaView, Button } from 'react-native'
+import AlertExample from './components/AlertExample'
+import AlertPractice from './components/AlertPractice'
+import ModelExample from './components/ModelExample'
+
+const App = () => {
+  return (
+    //<AlertExample/>
+    //<AlertPractice/>
+    <ModelExample/>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
